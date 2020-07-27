3e362abbe46c497ef1ae359b0caf2aee0c822845
Tyler Silcox <tylerrsilcox@gmail.com>
2020-07-27 01:29:51 -0400

    Fixed issue when user hadn't selected list view

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -14,7 +14,7 @@ import pandas as pd
 
 base_url = "https://www.bidfta.com"
 auction_id = 47327
-url = "https://www.bidfta.com/auctionItems?idauctions={auction_id}&pageId={page_id}"
+url = "https://www.bidfta.com/auctionItems?listView=true&idauctions={auction_id}&pageId={page_id}"
 
 driver = webdriver.Firefox()
 driver.implicitly_wait(3)
@@ -23,27 +23,30 @@ driver.get(url.format(auction_id=auction_id, page_id=1))
 page_source_1 = driver.page_source
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-items = soup.find_all("div", attrs={"class": "col-md-12 product-list listView "})
+items = soup.find_all("div", attrs={"class": "col-md-12 product-list listView"})
 
 more_items = True
 
 while more_items:
     try:
+        # Find next button
         but = driver.find_element_by_xpath("//span[@class='next']")
         but.click()
     except NoSuchElementException:
         more_items = False
         break
 
-    time.sleep(1)
+    time.sleep(2)
 
     soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-    found_items = soup.find_all("div", attrs={"class": "col-md-12 product-list listView "})
+    # Find all items on the page
+    found_items = soup.find_all("div", attrs={"class": "col-md-12 product-list listView"})
 
-    items = [*items, *found_items]
+    items.extend(found_items)
 
 for item in items:
+    # Print out partial url of the item
     partial_url = item.contents[1].contents[1].contents[1].contents[1].attrs["href"]
     partial_url = partial_url[:partial_url.find("&firstIdItem")]
     print(partial_url)
